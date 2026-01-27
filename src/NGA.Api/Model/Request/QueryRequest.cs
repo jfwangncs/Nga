2cb26f7b50af99450c261824b319cb17062bb71c
jfwang <jfwangncs@gmail.com>
2026-01-27 18:24:37 +0800

    update page size

--- src/NGA.Api/Model/Request/QueryRequest.cs
+++ src/NGA.Api/Model/Request/QueryRequest.cs
@@ -4,7 +4,7 @@ namespace NGA.Api.Model.Request
     {
         public int PageIndex { get; set; } = 1;
 
-        public int PageSize { get; set; } = 20;
+        public int PageSize { get; set; } = 10;
 
         public string SearchKey { get; set; } = "";
     }
